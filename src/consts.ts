1d6708d7e5517678742f765ca5d6bb4325fc9b92
Ashir Imran <71806327+xlinkfrostx@users.noreply.github.com>
2025-03-20 19:39:55 -0400

    Create base layout

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = "v1ris";
+export const SITE_DESCRIPTION =
+  "Portfolio for v1ris, a composer writing interactive music for narrative-driven games.";
